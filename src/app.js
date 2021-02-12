db9485dd2d1ca3c1f6bb4694563991195d25b633
Vinay Kardile <55827896+vinaykardile77@users.noreply.github.com>
2021-02-12 22:20:38 +0530

    Test

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox: Vinay</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+export default App;
