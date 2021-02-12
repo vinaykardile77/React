0a3bd94107315364d81a4c9a90c125945beec896
Vinay Kardile <55827896+vinaykardile77@users.noreply.github.com>
2021-02-12 22:06:35 +0530

    Changes Commited Changes Commited

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox: Vinay</h1>
+      <h1>1 Hello CodeSandbox: Vinay</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
